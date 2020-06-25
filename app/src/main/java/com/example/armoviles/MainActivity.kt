b9ea08c4c14c99c256ba4bfd8ee4c363f1d630cd
ulisesss123 <ulises_022898@hotmail.com>
2020-06-25 15:55:06 -0500

    Inicio de sesion

--- app/src/main/java/com/example/armoviles/MainActivity.kt
+++ app/src/main/java/com/example/armoviles/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
+
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
